017a1968ac63b3d7ff49baa3922b90d5b93f561f
17yange <17yange@smtexas.org>
2017-02-27 13:02:31 -0600

    Yeet

--- Elevens/src/Activity02  Starter Code/DeckTester.java
+++ Elevens/src/Activity02  Starter Code/DeckTester.java
@@ -7,7 +7,16 @@ public class DeckTester {
 	 * The main method in this class checks the Deck operations for consistency.
 	 *	@param args is not used.
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+        {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+            String[] ranks = {"jack", "queen", "king"};
+            String[] suits = {"blue", "red"};
+            int[] pointValues = {11, 12, 13};
+            Deck d = new Deck(ranks, suits, pointValues);
+            System.out.println(d.isEmpty());
+            System.out.println(d.size());
+            System.out.println(d.deal());
+            System.out.println(d.toString());
 	}
 }
